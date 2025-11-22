e1bdfb62fe75dcf266456fdeea2d3a4c6fec662b
Soumya Shikha <soumya@bdec.in>
2025-11-22 13:08:23 +0530

    fix compilation issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "chart-vendor": ["chart.js", "react-chartjs-2", "recharts"],
+          "map-vendor": ["react-simple-maps"],
+        },
+      },
+    },
+  },
 });
